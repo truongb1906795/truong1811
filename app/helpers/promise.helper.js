6932c7300a34297d4e8956c02d65e48cd501cba1
Le Van Truong <truongb1906795@student.ctu.edu.vn>
2022-03-09 21:51:43 -0800

    Cai dat xu ly loi

---
+++ app/helpers/promise.helper.js
@@ -0,0 +1,7 @@
+const handlerPromise = (promise) => {
+    return promise
+        .then((data) => [null, data])
+        .catch((error) => [error, undefined]);
+};
+
+module.exports = handlerPromise;
